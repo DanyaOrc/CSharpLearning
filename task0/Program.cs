0b37e471ee7b82566dd3068c6a9bf6be87b30062
Daniil Ivanov <danyaorc@gmail.com>
2023-02-17 21:23:33 +0300

    Reforge to correct and add zero task

---
+++ task0/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Разминка
+
+Напишите программу, которая на вход принимает число и выдаёт его квадрат (умножение на само себя).
+
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49 */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine("Квадрат числа " + number + " равен " + sqr);
