1bdb05d17562c2532d47de20acc88cdeea3feacc
Daniil Ivanov <danyaorc@gmail.com>
2023-02-22 19:02:15 +0300

    Add task 19 and 21

---
+++ task19/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 19. 
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+14212 -> нет 
+12821 -> да 
+23432 -> да */ 
+ 
+Console.Write("Введите число: "); 
+int num = Convert.ToInt32(Console.ReadLine()); 
+int rem, sum = 0, rev; 
+{ 
+rev = num; 
+    while (Convert.ToBoolean(num)) 
+        { 
+            rem = num % 10; 
+            num = num / 10; 
+            sum = sum * 10 + rem; 
+        } 
+    Console.WriteLine("Перевернутое число - это: {0} ", sum); 
+    if (rev == sum)  
+        { 
+            Console.WriteLine("Число является палиндромом"); 
+        } 
+    else 
+        { 
+            Console.WriteLine("Число не является палиндромом"); 
+        } 
+};
---
+++ task21/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 21. 
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве. 
+A (3,6,8); B (2,1,-7), -> 15.84 
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+ 
+double getDistance (double xa, double ya, double za, double xb, double yb, double zb) 
+{ 
+     return Math.Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za)); 
+} 
+Console.Write("Enter the x coordinate of the first point: "); 
+double xa = Convert.ToDouble(Console.ReadLine()); 
+Console.Write("Enter the y coordinate of the first point: "); 
+double ya = Convert.ToDouble(Console.ReadLine()); 
+Console.Write("Enter the z coordinate of the first point: "); 
+double za = Convert.ToDouble(Console.ReadLine()); 
+ 
+Console.Write("Enter the x coordinate of the second point: "); 
+double xb = Convert.ToDouble(Console.ReadLine()); 
+Console.Write("Enter the y coordinate of the second point: "); 
+double yb = Convert.ToDouble(Console.ReadLine()); 
+Console.Write("Enter the z coordinate of the second point: "); 
+double zb = Convert.ToDouble(Console.ReadLine()); 
+ 
+Console.WriteLine("Distance between points: " + Math.Round(getDistance(xa, ya, za, xb, yb, zb), 2)); 
